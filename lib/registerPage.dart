0ed1ee1eaef607978556ffdc8e163e3a477296f9
justnt47 <102816538+justnt47@users.noreply.github.com>
2024-02-15 17:29:39 +0700

    Update registerPage.dart

--- lib/registerPage.dart
+++ lib/registerPage.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// fsdfsdf
+
 class registPage extends StatefulWidget {
   const registPage({super.key});
 
