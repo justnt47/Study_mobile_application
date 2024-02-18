9e1de2c4e1172c443068002f00d1cf76bd31ee26
OME123456 <159910067+OME123456@users.noreply.github.com>
2024-02-18 15:07:25 +0700

    ทำสไลรูปภาพ

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slider/carousel.dart';
+import 'package:flutter_image_slider/indicator/Circle.dart';
 
 class home extends StatefulWidget {
-  home({super.key});
+  const home({Key? key}) : super(key: key);
 
   @override
   State<home> createState() => _homeState();
@@ -10,22 +12,45 @@ class home extends StatefulWidget {
 class _homeState extends State<home> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 290,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Center(
+          child: Text('LEARNING FUTURE', style: TextStyle(color: Colors.black)),
+        ),
+      ),
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Column(children: [Container(
+          height: size.height * 0.35,
+          width: size.width,
+          child: Carousel(
+            indicatorBarColor: Colors.black.withOpacity(0.2),
+            autoScrollDuration: Duration(seconds: 2),
+            animationPageDuration: Duration(milliseconds: 400),
+            activateIndicatorColor: Colors.black,
+            animationPageCurve: Curves.bounceInOut,
+            indicatorBarHeight: 50,
+            indicatorHeight: 20,
+            indicatorWidth: 20,
+            unActivatedIndicatorColor: Colors.grey,
+            stopAtEnd: true,
+            autoScroll: true,
+            items: [
+              Container(
+                child: Image.asset('images/anime1.jpg', fit: BoxFit.cover,),
               ),
-              Text(
-                "This is Homepage",
-                style: TextStyle(fontSize: 50),
+              Container(
+                child: Image.asset('images/anime2.jpg',fit: BoxFit.cover,),
               ),
-              Icon(Icons.abc)
+              Container(
+                child: Image.asset('images/anime3.jpg',fit: BoxFit.cover,),
+              )
             ],
           ),
-        ),
+        )]),
       ),
     );
   }
