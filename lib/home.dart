a9552e2d679c5ee317fcfc4ed12bab4c2772213d
OME123456 <159910067+OME123456@users.noreply.github.com>
2024-02-20 21:56:42 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image_slider/carousel.dart';
-import 'package:flutter_image_slider/indicator/Circle.dart';
 
 class home extends StatefulWidget {
   const home({Key? key}) : super(key: key);
@@ -16,7 +15,7 @@ class _homeState extends State<home> {
     'การจัดสถานะใน Flutter',
   ];
   String? selectedCourse;
-  
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -30,101 +29,121 @@ class _homeState extends State<home> {
       body: Container(
         height: size.height,
         width: size.width,
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Container(
-                height: size.height * 0.35,
-                width: size.width,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 3,
-                      blurRadius: 7,
-                      offset: Offset(0, 3),
+            SizedBox(height: 20),
+            Container(
+              height: size.height * 0.25,
+              width: size.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Carousel(
+                indicatorBarColor: Colors.black.withOpacity(0.2),
+                autoScrollDuration: Duration(seconds: 2),
+                animationPageDuration: Duration(milliseconds: 400),
+                activateIndicatorColor: Colors.white,
+                animationPageCurve: Curves.easeInOut,
+                indicatorBarHeight: 50,
+                indicatorHeight: 20,
+                indicatorWidth: 20,
+                unActivatedIndicatorColor: Colors.grey.withOpacity(0.5),
+                stopAtEnd: true,
+                autoScroll: false,
+                items: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  ],
-                ),
-                child: Carousel(
-                  indicatorBarColor: Colors.black.withOpacity(0.2),
-                  autoScrollDuration: Duration(seconds: 2),
-                  animationPageDuration: Duration(milliseconds: 400),
-                  activateIndicatorColor: Colors.white,
-                  animationPageCurve: Curves.easeInOut,
-                  indicatorBarHeight: 50,
-                  indicatorHeight: 20,
-                  indicatorWidth: 20,
-                  unActivatedIndicatorColor: Colors.grey.withOpacity(0.5),
-                  stopAtEnd: true,
-                  autoScroll: false,
-                  items: [
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset(
-                          'images/anime1.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child:
+                          Image.asset('images/anime1.jpg', fit: BoxFit.cover),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset(
-                          'images/anime2.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: Image.asset(
-                          'images/anime3.jpg',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child:
+                          Image.asset('images/anime2.jpg', fit: BoxFit.cover),
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child:
+                          Image.asset('images/anime3.jpg', fit: BoxFit.cover),
+                    ),
+                  ),
+                ],
               ),
             ),
+            SizedBox(height: 40),
+            Text(
+              'แนะนำคอร์สเรียน',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
             SizedBox(height: 20),
-            Text('แนะนำคอร์สเรียน',
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-            SizedBox(height: 20),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: courses.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(courses[index]),
-                  trailing: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        selectedCourse = courses[index];
-                        print('สมัครคอร์สเรียน: ${courses[index]}');
-                      });
-                    },
-                    child: Text('เริ่มเรียนคอร์สนี้',
+            Expanded(
+              child: ListView.builder(
+                itemCount: courses.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 3,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: ListTile(
+                      title: Text(
+                        courses[index],
                         style: TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold)),
-                  ),
-                );
-              },
-            ),        
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedCourse = courses[index];
+                            print('สมัครคอร์สเรียน: ${courses[index]}');
+                          });
+                        },
+                        style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blue),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'เริ่มเรียน',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
