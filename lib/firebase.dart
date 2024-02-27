d499fbf5c98f3fb4576a0e276bd953014a3d3907
justnt47 <nt.nanthiphat@gmail.com>
2024-02-27 09:46:20 +0700

    test

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +10,20 @@ Future<void> userSetup(String displayname) async {
   final uid = auth.currentUser?.uid.toString();
   // print(uid);
   // print(displayname);
-  users.add({"displayName": displayname, "uid": uid});
+  var result = await users.add({"displayName": displayname, "uid": uid});
+
+  return print(
+      "added username: ${displayname} Uid: ${uid} DocID: ${result.id}");
+}
+
+/* Future<void> saveBookmark(String displayname) async {
+  CollectionReference user = FirebaseFirestore.instance.collection("Users");
+  DocumentReference BookRef = user.collection("Users").document
+  FirebaseAuth auth = FirebaseAuth.instance;
+  final uid = auth.currentUser?.uid.toString();
+  // print(uid);
+  // print(displayname);
+  bookmarkRef.add({"displayName": displayname, "uid": uid});
 
   return;
-}
+} */
