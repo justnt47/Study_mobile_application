82aa1af7686553e21c961e00cbeb53be4d5e54ca
OME123456 <chayanon.deej@ku.th>
2024-03-07 20:22:46 +0700

    update

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:test_any_code/firebase.dart';
+import 'package:test_any_code/bookMarkPage.dart';
+import 'package:test_any_code/home.dart';
+import 'package:test_any_code/lessonPage.dart';
+import 'package:test_any_code/settingPage.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-import './home.dart';
-import './lessonPage.dart';
-import './bookMarkPage.dart';
-import './settingPage.dart';
 
 class homePage extends StatefulWidget {
   homePage({super.key});
@@ -23,21 +24,20 @@ class _homePageState extends State<homePage> {
     bookmark(),
     sett(),
   ];
+
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: CircleAvatar(
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Color.fromARGB(255, 118, 200, 241),
-        child: IconButton(
-          icon: Icon(
-            Icons.notifications_on,
-            color: Colors.yellow,
-          ),
-          onPressed: () {
-            setState(() {
-              printDoc();
-            });
-          },
+        onPressed: () {
+          _showFeedbackDialog(context);
+        },
+        child: Icon(
+          Icons.feedback,
+          color: Colors.yellow,
         ),
       ),
       body: mobileScreens[screenIndex],
@@ -50,64 +50,100 @@ class _homePageState extends State<homePage> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             IconButton(
-                onPressed: () {
-                  setState(() {
-                    //------ กำหนดค่า Index เมื่อมีการคลิก ------
-                    screenIndex = 0;
-                  });
-                },
-                icon: Icon(
-                  Icons.home,
-                  size: 35,
-                  //------ ถ้า Index = 0 ให้ไอคอนสีเหลือง ถ้าไม่ใช้ไอคอนสีขาว ------
-                  color: screenIndex == 0
-                      ? Color.fromRGBO(214, 214, 214, 1)
-                      : Colors.white,
-                  // color: Colors.white,
-                )),
+              onPressed: () {
+                setState(() {
+                  //------ กำหนดค่า Index เมื่อมีการคลิก ------
+                  screenIndex = 0;
+                });
+              },
+              icon: Icon(
+                Icons.home,
+                size: 35,
+                color: screenIndex == 0
+                    ? Color.fromRGBO(214, 214, 214, 1)
+                    : Colors.white,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  setState(() {
-                    screenIndex = 1;
-                  });
-                },
-                icon: Icon(
-                  Icons.menu_book_sharp,
-                  size: 35,
-                  color: screenIndex == 1
-                      ? Color.fromRGBO(214, 214, 214, 1)
-                      : Colors.white,
-                  // color: Colors.white,
-                )),
+              onPressed: () {
+                setState(() {
+                  screenIndex = 1;
+                });
+              },
+              icon: Icon(
+                Icons.menu_book_sharp,
+                size: 35,
+                color: screenIndex == 1
+                    ? Color.fromRGBO(214, 214, 214, 1)
+                    : Colors.white,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  setState(() {
-                    screenIndex = 2;
-                  });
-                },
-                icon: Icon(
-                  Icons.bookmark,
-                  size: 35,
-                  color: screenIndex == 2
-                      ? Color.fromRGBO(214, 214, 214, 1)
-                      : Colors.white,
-                )),
+              onPressed: () {
+                setState(() {
+                  screenIndex = 2;
+                });
+              },
+              icon: Icon(
+                Icons.bookmark,
+                size: 35,
+                color: screenIndex == 2
+                    ? Color.fromRGBO(214, 214, 214, 1)
+                    : Colors.white,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  setState(() {
-                    screenIndex = 3;
-                  });
-                },
-                icon: Icon(
-                  Icons.settings,
-                  size: 35,
-                  color: screenIndex == 3
-                      ? Color.fromRGBO(214, 214, 214, 1)
-                      : Colors.white,
-                )),
+              onPressed: () {
+                setState(() {
+                  screenIndex = 3;
+                });
+              },
+              icon: Icon(
+                Icons.settings,
+                size: 35,
+                color: screenIndex == 3
+                    ? Color.fromRGBO(214, 214, 214, 1)
+                    : Colors.white,
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showFeedbackDialog(BuildContext context) {
+    TextEditingController feedbackController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Provide Feedback'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Please provide your feedback here:'),
+                TextField()
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                
+              },
+              child: Text('Submit'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
